c1ee90476d7db1ec9ec42209536e46e9b3956f8b
Your Name <you@example.com>
2022-11-19 12:25:32 -0500

    Added Player, EnemyShip classes

--- temp.py
+++ temp.py
@@ -22,6 +22,9 @@ BLUE_SPACE_LASER = py.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 GREEN_SPACE_LASER = py.image.load(os.path.join("assets", "pixel_laser_green.png"))
 YELLOW_SPACE_LASER = py.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
+TRASHCAN = py.image.load(os.path.join("assets", "trash.png"))
+
+#BACKGROUND = py.image.load(os.path.join("assets", "Oahu2.png"))
 BACKGROUND = py.image.load(os.path.join("assets", "background-black.png"))
 BACKGROUND_SCALED = py.transform.scale(BACKGROUND, (WIDTH, HEIGHT))
 
@@ -37,7 +40,40 @@ class Ship:
         self.cool_down_counter = 0
 
     def draw(self, window):
-        py.draw.rect(window, (0, 255,0), (self.x_pos, self.y_pos, 50, 50), width=0)
+        #py.draw.rect(window, (0, 255,0), (self.x_pos, self.y_pos, 50, 50), width=0)
+        window.blit(self.ship_image, (self.x_pos, self.y_pos))
+
+    def get_width(self):
+        return self.ship_image.get_width()
+        
+    def get_height(self):
+        return self.ship_image.get_height()
+
+
+class Player(Ship):
+    def __init__(self, x_pos, y_pos, health=100) -> None:
+        super().__init__(x_pos, y_pos, health)
+        self.ship_image = YELLOW_SPACE_SHIP
+        self.laser_image = BLUE_SPACE_LASER
+        self.mask = py.mask.from_surface(self.ship_image)
+        self.max_health = 100
+
+class EnemyShip(Ship):
+    COLOR_MAP = {
+
+        "red": (RED_SPACE_SHIP, RED_SPACE_LASER),
+        "blue": (BLUE_SPACE_SHIP, BLUE_SPACE_LASER),
+        "green": (GREEN_SPACE_SHIP, GREEN_SPACE_LASER),
+
+    }
+
+    def __init__(self, x_pos, y_pos, color,health=100) -> None:
+        super().__init__(x_pos, y_pos, health)
+        self.ship_image, self.laser_image = self.COLOR_MAP[color]
+        self.mask = py.mask.from_surface(self.ship_image)
+
+    def move(self, vel):
+        self.y_pos += vel
 
 
 def main():
@@ -45,10 +81,12 @@ def main():
     FPS = 60
     level = 1
     lives = 5
+    player_vel = 5
     main_font = py.font.SysFont('comicsans', 35)
     clock = py.time.Clock()
 
-    s = Ship(300, 200)
+#   s = Ship(300, 200)
+    p = Player(300, 200)
 
     def redraw_window():
         WIN.blit(BACKGROUND_SCALED, (0, 0))
@@ -57,7 +95,7 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_lable, (WIDTH - level_lable.get_width() - 10, 10))
 
-        s.draw(WIN)
+        p.draw(WIN)
 
         py.display.update()
 
@@ -69,6 +107,15 @@ def main():
         for event in py.event.get():
             if event.type == py.QUIT:
                 run = False
+        keys = py.key.get_pressed()
+        if keys[py.K_a] and p.x_pos - player_vel > 0: # moves left
+            p.x_pos -= player_vel
+        if keys[py.K_d] and p.x_pos + player_vel + p.get_width() < WIDTH:
+            p.x_pos += player_vel
+        if keys[py.K_w] and p.y_pos - player_vel > 0: # moves down
+            p.y_pos -= player_vel
+        if keys[py.K_s]and p.y_pos + player_vel + p.get_height() < HEIGHT:  # move up
+            p.y_pos += player_vel 
 main()
 
 
